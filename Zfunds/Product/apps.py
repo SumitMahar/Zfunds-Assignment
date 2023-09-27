8e0c812d73923d287bd013e4700316ef2c0c334c
Sumit <SumitMahar78@gmail.com>
2023-09-27 09:02:36 +0530

    added product app

--- Zfunds/Product/apps.py
+++ Zfunds/Product/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ZfundscoreConfig(AppConfig):
+class ProductConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "ZfundsCore"
+    name = "Zfunds.Product"
---
+++ Zfunds/Product/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    class Meta:
+        db_table = "category" # Table name in database
+        verbose_name_plural = "Categories" # Plural name for the model
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        db_table = "product"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} | {self.category}"
+    
+
+
